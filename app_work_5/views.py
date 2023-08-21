461ef89da901b462a6ba0366cf128ba32f77a75a
_K1rl_ <126946536+1K1rl1@users.noreply.github.com>
2023-08-21 13:23:27 +0500

    work_10

--- app_work_5/views.py
+++ app_work_5/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Advertisement_2
+from .forms import AdvForms
+from django.urls import reverse
 def index(request):
     advertisements = Advertisement_2.objects.all()
     context = {'advertisements': advertisements}
@@ -8,3 +10,19 @@ def index(request):
     
 def top_sellers(request):
     return render(request, 'top-sellers.html')
+def adv_post(request):
+    #form = AdvForms()
+    #context = {'form':form  }
+    #return render(request, 'advertisement-post.html', context)
+    if request.method == 'POST':
+        form = AdvForms(request.POST, request.FILES)
+        if form.is_valid():
+            advs = Advertisement_2(**form.cleaned_data)
+            advs.user = request.user
+            advs.save()
+            url = reverse('main-page')
+            return redirect(url)
+    else:
+        form = AdvForms()
+        context = {'form':form}
+    return render(request, 'advertisement-post.html', context)
